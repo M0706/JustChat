9898f13dec9df7188c8bcd4e62ece1de11a88309
Manav Garg <manavgarg@Manavs-MacBook-Air.local>
2020-11-24 23:08:11 +0530

    Added backend of the Chatroom in All contacts list

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -9,6 +9,12 @@ import { User } from "../../types";
 import styles from "./style";
 import moment from "moment";
 import { useNavigation } from '@react-navigation/native';
+import { API, 
+  graphqlOperation, 
+  Auth} from "aws-amplify";
+
+import {createChatRoom, 
+  createChatRoomUser} from '../../graphql/mutations'
 
 export type ContactListItemProps = {
   user: User;
@@ -19,8 +25,62 @@ const ContactListItem = (props: ContactListItemProps) => {
 
   const navigation = useNavigation();
 
-  const onClick = () => {
+  const onClick = async() => {
    // navigate to chat room with this user
+   try{
+
+    //  1. Create a new Chat Room
+    const newChatRoomData = await API.graphql(
+      graphqlOperation(
+        createChatRoom, {
+          input: {
+            //lastMessageID: "zz753fca-e8c3-473b-8e85-b14196e84e16"
+          }
+        }
+      )
+    )
+
+    if (!newChatRoomData.data) {
+      console.log(" Failed to create a chat room");
+      return;
+    }
+
+    const newChatRoom = newChatRoomData.data.createChatRoom;
+
+    // 2. Add `user` to the Chat Room
+    await API.graphql(
+      graphqlOperation(
+        createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          }
+        }
+      )
+    )
+    
+    //  3. Add authenticated user to the Chat Room
+    const userInfo = await Auth.currentAuthenticatedUser();
+    await API.graphql(
+      graphqlOperation(
+        createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          }
+        }
+      )
+    )
+
+    navigation.navigate('ChatRoom',{
+      id: newChatRoom.id,
+      name:""
+    })
+
+   }catch(e){
+     console.log(e)
+   }
+
   }
 
   return (
