6b2e353964b660526429cb6a22f923dee5940745
Manav Garg <manavgarg@Manavs-MacBook-Air.local>
2020-11-28 22:02:44 +0530

    Message sending

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {FlatList, Text, ImageBackground } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
@@ -7,16 +7,45 @@ import chatRoomData from '../data/Chats';
 import ChatMessage from "../components/ChatMessage";
 import BG from '../assets/images/BG.png';
 import InputBox from "../components/InputBox";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import { messagesByChatRoom } from "../graphql/queries";
 
 const ChatRoomScreen = () => {
 
+  const [messages, setMessages] = useState([])
+  const [myId, setMyId] = useState(null);
   const route = useRoute();
+  //console.log("Route:",route);
+
+  useEffect(()=>{
+    const fetchMessages = async()=>{
+      const messagesData = await API.graphql(
+        graphqlOperation(
+          messagesByChatRoom,{
+            chatRoomID:route.params.id,
+            sortDirection: "DESC"
+          }
+        )
+      )
+      //console.log("MessagesData ====>", messagesData)
+      setMessages(messagesData.data.messagesByChatRoom.items);
+    }
+    fetchMessages();
+  },[])
+
+  useEffect(() => {
+    const getMyId = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      setMyId(userInfo.attributes.sub);
+    }
+    getMyId();
+  }, [])
 
   return (
     <ImageBackground style={{width: '100%', height: '100%'}} source={BG}>
       <FlatList
-        data={chatRoomData.messages}
-        renderItem={({ item }) => <ChatMessage message={item} />}
+        data={messages}
+        renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
         inverted
       />
 
@@ -26,3 +55,15 @@ const ChatRoomScreen = () => {
 }
 
 export default ChatRoomScreen;
+
+
+
+
+
+
+
+
+
+
+
+
