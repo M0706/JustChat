b460d4555fe7f78119105a2c47e706da55b3c195
Chris Marshall <c.marshall.engineer@gmail.com>
2021-01-10 00:18:54 +0000

    can store registered user in user graphql api

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -7,13 +7,56 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 import { withAuthenticator } from 'aws-amplify-react-native';
-import Amplify from 'aws-amplify';
+import Amplify, {
+  Auth,
+  API,
+  graphqlOperation
+} from 'aws-amplify';
+import {
+  getUser
+} from './graphql/queries';
+import {
+  createUser
+} from './graphql/mutations';
 import config from './aws-exports';
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser,
+            { id: userInfo.attributes.sub }
+          )
+        );
+
+        if (userData.data.getUser) {
+          console.log('User registered');
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/3.jpg',
+          status: 'Hey, Im a tree'
+        };
+
+        await API.graphql(
+          graphqlOperation(createUser, { input: newUser })
+        );
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
