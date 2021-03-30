2a3c01460d1035b24441d15f6714b842e2082f27
Harshit Gupta <harshitgupta1023@gmail.com>
2021-03-30 19:17:59 +0530

    minor changes

--- screens/Camera/styles.ts
+++ screens/Camera/styles.ts
@@ -1,48 +1,48 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "black",
-  },
-  preview: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: "center",
-    margin: 20,
-  },
-  buttonTakePic: {
-    alignSelf: "center",
-    marginVertical: 10,
-    height: 50,
-    width: 50,
-    borderRadius: 25,
-    backgroundColor: "black",
-  },
+      container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+      },
+      preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      },
+      capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+      },
+      buttonTakePic: {
+        alignSelf: 'center',
+        marginVertical: 10,
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        backgroundColor: 'Black',
+      },
   buttonRecord: {
-    alignSelf: "center",
+    alignSelf: 'center',
     marginVertical: 10,
     height: 50,
     width: 50,
     borderRadius: 25,
-    backgroundColor: "#ff4343",
+    backgroundColor: '#ff4343',
   },
   buttonStop: {
-    alignSelf: "center",
+    alignSelf: 'center',
     marginVertical: 20,
     height: 30,
     width: 30,
     borderRadius: 3,
-    backgroundColor: "#ff4343",
+    backgroundColor: '#ff4343',
   },
 });
 
